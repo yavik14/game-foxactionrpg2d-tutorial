cdb7ee0b62ccd4f32ed1ba7909eae0ae37e781c5
The YaviK Team <theyavikteam@gmail.com>
2024-12-31 08:54:08 +0100

    Player basic movement

--- code/player.gd
+++ code/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 @export var speed: int = 4
-var move_vector = Vector2.ZERO
+var move_vector: Vector2 = Vector2.ZERO
 
-func _physics_process(delta):
+func _physics_process(_delta: float):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
