bc9d1086af4cca2826a541546378ec69787c121c
Fco Javier Rodríguez Ponce <6715477+yavik14@users.noreply.github.com>
2025-01-02 20:30:08 +0100

    Player basic animation (#5)
    Co-authored-by: The YaviK Team <theyavikteam@gmail.com>

--- code/player.gd
+++ code/player.gd
@@ -6,6 +6,11 @@ extends CharacterBody2D
 
 var move_vector: Vector2 = Vector2.ZERO
 
+@onready var animation_player = null
+
+func _ready():
+	animation_player = $AnimationPlayer
+
 func _physics_process(delta: float):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,8 +18,13 @@ func _physics_process(delta: float):
 	input_vector = input_vector.normalized()
 
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animation_player.play("run_right")
+		else:
+			animation_player.play("run_left")
 		move_vector = move_vector.move_toward(input_vector * max_speed, acceleration * delta)
 	else:
+		animation_player.play("idle_right")
 		move_vector = move_vector.move_toward(Vector2.ZERO, friction * delta) 
 
 	velocity = move_vector
